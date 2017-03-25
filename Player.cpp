8da4c4fdeeb36bea280b3dc7fc6a015dcda2624f
hp561 <hp561@ufl.edu>
2017-03-25 15:59:52 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -6,5 +6,30 @@
  */
 
 #include "mono.h"
+#include<string>
+
+#include <iostream>
+using namespace std;
+
+class player{
+
+    private:
+    string name;
+    string typeOfPlace;
+    int currentMoney;
+    vector<Property> properties;
+    
+    public:
+    player(string name, string piece, int money);
+    bool hasMoney(Property property);
+    void addHouse();
+    string getPlayer();
+    void setPlayer(string name);
+    void afkCheck();
+    bool checkMoneyForBuying();
+    void removeProperty();
+    void addProperty();
+    
 
 
+};
