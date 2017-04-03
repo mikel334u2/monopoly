e8ccd8f55605392d419c24e967e8510ee34b243a
mikel334u2 <mikel53377@gmail.com>
2017-04-02 22:21:01 -0400

    commenting out boardView
    so that i can see how it fits with main class

--- boardView.cpp
+++ boardView.cpp
@@ -1,32 +1,42 @@
+/*
+ * boardView.cpp
+ *
+ *  Created on: Apr 2, 2017
+ *      Author: mikel
+ */
+
 #include <iostream>
 #include <string>
+#include "mono.h"
 
-class boardView
-{
-	private:
-		string property;
-		int location;
+//class boardView
+//{
+//	private:
+//		string property;
+//		int location;
+//
+//	public:
+//
+//		string getProperty()
+//		{
+//			return property;
+//		}
+//
+//		int getLocation()
+//		{
+//			return location;
+//		}
+//
+//		void setProperty(string property)
+//		{
+//			this.property = property;
+//		}
+//
+//		void setLocation(int location)
+//		{
+//			this.location = location;
+//		}
+//
+//}
 
-	public:
-	
-		string getProperty()
-		{
-			return property;
-		}
 
-		int getLocation()
-		{
-			return location;
-		}
-
-		setProperty(string property)
-		{
-			this.property = property;
-		}
-
-		setLocation(int location)
-		{
-			this.location = location;
-		}	
-
-}
